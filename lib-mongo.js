2128e2a7d1df91130c9fa0ab0d815008a484fc54
Osmar <osmarmolina39@aragon.unam.mx>
2020-10-28 22:27:32 -0600

    Pendiente de URI

--- lib-mongo.js
+++ lib-mongo.js
@@ -6,17 +6,18 @@ const USER = encodeURIComponent(config.dbUser)
 const PASSWORD = encodeURIComponent(config.dbPassword)
 const DB_NAME = config.dbName;
 
-const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`
+
 
 class MongoLib {
   constructor() {
-    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true })
-    this.dbName = DB_NAME;
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser:true, useUnifiedTopology:true });
+    this.dbName = DB_NAME
   }
 
-  async connect() {
+  connect() {
     if (!MongoLib.connection) {
-      MongoLib.connection = await((resolve, reject) => {
+      MongoLib.connection = new Promise((resolve, reject) => {
         this.client.connect(err => {
           if (err) {
             reject(err)
@@ -35,21 +36,21 @@ class MongoLib {
 
   get(collection, id) {
     return this.connect().then(db => {
-      return db().collection(collection).findOne({ _id: ObjectId(id) })});
+      return db.collection(collection).findOne({ _id: ObjectId(id) })});
   }
   create(collection, data) {
     return this.connect().then(db => {
-        return db().collection(collection).insertOne(data) }) .then(result => result.insertedId)
+        return db.collection(collection).insertOne(data) }) .then(result => result.insertedId)
   }
 
   update(collection, id, data) {
     return this.connect().then(db => {
-        return db().collection(collection).updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true })}).then(result => result.upsertedId || id)
+        return db.collection(collection).updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true })}).then(result => result.upsertedId || id)
   }
 
   delete(collection, id) {
     return this.connect().then(db => {
-        return db().collection(collection).deleteOne({ _id: ObjectId(id) })}) .then(() => id)}
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) })}) .then(() => id)}
 }
 
 module.exports = MongoLib
